bc7cfdb9c233433855c0dc25049638f38a2e9591
KReysen <43965763+KReysen@users.noreply.github.com>
2019-10-09 13:22:41 -0600

    list resource created

--- src/controllers/listController.js
+++ src/controllers/listController.js
@@ -1,5 +1,13 @@
+const listQueries = require("../db/queries.lists.js");
+
 module.exports = {
     index(req, res, next){
-        res.send("TODO: list all grocery lists");
+        listQueries.getAllLists((err, lists) => {
+            if(err){
+                res.redirect(500, "static/index");
+            } else {
+                res.render("lists/index", {lists});
+            }
+        })
     }
 }
